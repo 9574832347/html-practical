ee0554f951c46c3fd24a482d4b48f6624b1f9db9
9574832347 <121388869+9574832347@users.noreply.github.com>
2024-01-14 13:16:03 +0530

    Add files via upload

---
+++ Javascript assignment/javascript practical/script.js
@@ -0,0 +1,16 @@
+function show_password() {
+
+
+
+    let pass = document.querySelector("#pass")
+
+    if (pass.type == "password") {
+        pass.type = "text"
+        return false
+    } else {
+        pass.type = "password"
+        return false
+    }
+
+}
+
